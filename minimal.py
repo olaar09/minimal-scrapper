c96c6087853839fd4a7aeaf6886a3e0ee1b9a08f
olaar09 <agboolar09@gmail.com>
2017-07-26 21:06:35 +0100

    can save to file

--- minimal.py
+++ minimal.py
@@ -1,29 +1,37 @@
 import sys
 import scapper
+import multiprocessing
+
+output = multiprocessing.Queue()
 
 class minimal:
 
-    def __init__(self, args):
+    def __init__(self):
         sys.stdout.write('Starting minimal scrapper \n')
-        if len(args) < 2:
-            sys.stdout.write('Could not start program, expected "minimal.py <url>" \n')
-            sys.exit()
-        else:
-            self.args = args
 
-    def startScapper(self):
+    def startScapper(self, url):
         sys.stdout.write('Getting contents of  url :')
-        scrape = scapper.ScrapeUrl(self.args[1])
+        scrape = scapper.ScrapeUrl(url)
 
-        sys.stdout.write( self.args[1]+ '\n')
+        sys.stdout.write( url+ '\n')
         scrape.getSource()
 
         sys.stdout.write("Scrapping emails from content \n")
-        return scrape.scrapeEmails()
-
+        out = scrape.scrapeEmails().__str__()
+        if len(out) > 2:
+            scrape.saveData(out)
+        return
 
 
 
 if __name__ == '__main__':
-    minimalScraper = minimal(sys.argv)
-    print(minimalScraper.startScapper())
+    if len(sys.argv) < 2:
+        sys.stdout.write('Could not start program, expected "minimal.py <url>" \n')
+        sys.exit()
+
+    minimalScraper = minimal()
+    for url in  sys.argv[1:]:
+        multiprocessing.Process(target=minimalScraper.startScapper(url)).start()
+        print("Started thread for "+ url)
+
+
--- scapper.py
+++ scapper.py
@@ -1,5 +1,7 @@
-import urllib.request as request
+import urllib.request as request, urllib.error as reqErr
+import sys
 import re
+import time
 
 class ScrapeUrl:
 
@@ -9,10 +11,22 @@ class ScrapeUrl:
 
 
     def getSource(self):
-        respons =  request.urlopen(self.__url)
-        self.__htmlContent =  respons.read().decode('utf-8')
+        headers = {}
+        headers[
+            'User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+        req = request.Request(self.__url, headers=headers)
+        try:
+            resp = request.urlopen(req)
+            self.__htmlContent = resp.read().decode('utf-8')
+        except  reqErr.URLError as e: sys.stderr.write("Error reading url "+self.__url+ " Trace: "+ e.reason);
 
 
 
     def scrapeEmails(self):
         return re.findall(r'[\w\.-]+@[\w\.-]+', self.__htmlContent)
+
+
+    def saveData(self, data):
+        with open("data/"+str(time.time())+".txt", "w") as file:
+            file.write(data)
+
